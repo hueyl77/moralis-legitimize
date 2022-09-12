f62de81902b9f8b97044494ab52b8378d09c8352
Huey Ly <huey@webmail.us>
2022-09-12 09:40:43 +0800

    updated gitignore for secret files

--- _ethereum/config.js
+++ _ethereum/config.js
@@ -1,2 +1,4 @@
-export const legitSignatureAddress = "0x0ca9e07297dB443efa5Ca947407822A676CDc62b";
-export const legitNFTAddress = "0x5941E9299108810A3ecd1AAd8250989C6dEe7797";
+
+    export const legitSignatureAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+    export const legitNFTAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  
